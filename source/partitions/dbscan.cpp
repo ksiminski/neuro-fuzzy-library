c331e055948571c58bc3f086ba12981ba168dac6
Suchar230 <dsj230@gmail.com>
2022-04-14 20:51:10 +0200

    Indent using tabs

--- source/partitions/dbscan.cpp
+++ source/partitions/dbscan.cpp
@@ -9,111 +9,111 @@
 #include "../metrics/metric-euclidean.h"
 
 ksi::dbscan::dbscan()
-    : _metric_object(ksi::metric_euclidean().clone())
+	: _metric_object(ksi::metric_euclidean().clone())
 {
 }
 
 ksi::dbscan::dbscan(const double epsilon, const int minPoints, const ksi::metric &metric_object)
-    : _metric_object(metric_object.clone())
+	: _metric_object(metric_object.clone())
 {
-    this->_epsilon = epsilon;
-    this->_minPoints = minPoints;
+	this->_epsilon = epsilon;
+	this->_minPoints = minPoints;
 }
 
 ksi::partition ksi::dbscan::doPartition(const ksi::dataset &ds)
 {
-    try
-    {
-        // cluster
-        int C = -1;
+	try
+	{
+		// cluster
+		int C = -1;
 
-        std::size_t dsSize = ds.getNumberOfData();
+		std::size_t dsSize = ds.getNumberOfData();
 
-        // partition matrix
-        std::vector<std::vector<double>> mU;
+		// partition matrix
+		std::vector<std::vector<double>> mU;
 
-        std::vector<DatumState> datumStates(dsSize, UNDEFINED);
+		std::vector<DatumState> datumStates(dsSize, UNDEFINED);
 
-        for (std::size_t i = 0; i < dsSize; ++i)
-        {
-            if (datumStates[i] != UNDEFINED)
-                continue;
+		for (std::size_t i = 0; i < dsSize; ++i)
+		{
+			if (datumStates[i] != UNDEFINED)
+				continue;
 
-            const ksi::datum *datum = ds.getDatum(i);
+			const ksi::datum *datum = ds.getDatum(i);
 
-            auto neighbors = findNeighbors(datum, ds);
-            auto neighborsSize = neighbors.size();
+			auto neighbors = findNeighbors(datum, ds);
+			auto neighborsSize = neighbors.size();
 
-            if (neighborsSize < this->_minPoints)
-            {
-                datumStates[i] = NOISE;
-                continue;
-            }
+			if (neighborsSize < this->_minPoints)
+			{
+				datumStates[i] = NOISE;
+				continue;
+			}
 
-            ++C;
-            mU.push_back(std::vector<double>(dsSize, 0.0));
-            // cluster membership
-            mU[C][i] = 1.0;
-            datumStates[i] = CLUSTER_MEMBER;
+			++C;
+			mU.push_back(std::vector<double>(dsSize, 0.0));
+			// cluster membership
+			mU[C][i] = 1.0;
+			datumStates[i] = CLUSTER_MEMBER;
 
-            for (std::size_t j = 0; j < neighborsSize; ++j)
-            {
-                auto neighbor = neighbors[j].first;
-                auto neighborIndex = neighbors[j].second;
+			for (std::size_t j = 0; j < neighborsSize; ++j)
+			{
+				auto neighbor = neighbors[j].first;
+				auto neighborIndex = neighbors[j].second;
 
-                if (datumStates[neighborIndex] == NOISE)
-                {
-                    mU[C][neighborIndex] = 1.0;
-                    datumStates[neighborIndex] = CLUSTER_MEMBER;
-                }
-                if (datumStates[neighborIndex] != UNDEFINED)
-                    continue;
+				if (datumStates[neighborIndex] == NOISE)
+				{
+					mU[C][neighborIndex] = 1.0;
+					datumStates[neighborIndex] = CLUSTER_MEMBER;
+				}
+				if (datumStates[neighborIndex] != UNDEFINED)
+					continue;
 
-                mU[C][neighborIndex] = 1.0;
-                datumStates[neighborIndex] = CLUSTER_MEMBER;
+				mU[C][neighborIndex] = 1.0;
+				datumStates[neighborIndex] = CLUSTER_MEMBER;
 
-                auto neighbors_ = findNeighbors(neighbor, ds);
-                auto neighbors_Size = neighbors_.size();
+				auto neighbors_ = findNeighbors(neighbor, ds);
+				auto neighbors_Size = neighbors_.size();
 
-                if (neighbors_Size >= this->_minPoints)
-                {
-                    // concatenate
-                    neighbors.insert(neighbors.end(), neighbors_.begin(), neighbors_.end());
-                    neighborsSize += neighbors_Size;
-                }
-            }
-        }
+				if (neighbors_Size >= this->_minPoints)
+				{
+					// concatenate
+					neighbors.insert(neighbors.end(), neighbors_.begin(), neighbors_.end());
+					neighborsSize += neighbors_Size;
+				}
+			}
+		}
 
-        ksi::partition part;
-        part.setPartitionMatrix(mU);
-        return part;
-    }
-    CATCH
+		ksi::partition part;
+		part.setPartitionMatrix(mU);
+		return part;
+	}
+	CATCH
 }
 
 std::vector<std::pair<const ksi::datum *, std::size_t>> ksi::dbscan::findNeighbors(const ksi::datum *datum, const ksi::dataset &ds)
 {
-    std::vector<std::pair<const ksi::datum *, std::size_t>> neighbors;
-    std::size_t dsSize = ds.getNumberOfData();
+	std::vector<std::pair<const ksi::datum *, std::size_t>> neighbors;
+	std::size_t dsSize = ds.getNumberOfData();
 
-    for (std::size_t i = 0; i < dsSize; i++)
-    {
-        auto datum_ = ds.getDatum(i);
+	for (std::size_t i = 0; i < dsSize; i++)
+	{
+		auto datum_ = ds.getDatum(i);
 
-        if (this->_metric_object->calculateDistance(*datum, *datum_) <= this->_epsilon)
-            neighbors.emplace_back(datum_, i);
-    }
-    return neighbors;
+		if (this->_metric_object->calculateDistance(*datum, *datum_) <= this->_epsilon)
+			neighbors.emplace_back(datum_, i);
+	}
+	return neighbors;
 }
 
 ksi::partitioner *ksi::dbscan::clone() const
 {
-    return new dbscan(*this);
+	return new dbscan(*this);
 }
 
 std::string ksi::dbscan::getAbbreviation() const
 {
-    return std::string("dbscan");
+	return std::string("dbscan");
 }
 
 ksi::dbscan::~dbscan()
