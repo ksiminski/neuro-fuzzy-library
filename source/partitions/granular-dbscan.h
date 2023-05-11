12a207f882a2bd30849314da937e297bea53a6bd
Suchar230 <dsj230@gmail.com>
2023-05-11 17:53:08 +0200

    Rename parameters

--- source/partitions/granular-dbscan.h
+++ source/partitions/granular-dbscan.h
@@ -48,8 +48,8 @@ namespace ksi
         granular_dbscan(
             const double epsilon,
             const double minPoints,
-            const double mi1,
-            const double mi2,
+            const double xi,
+            const double psi,
             const partitioner &fuzzyficationAlgorihm,
             const s_norm &snorm,
             const t_norm &tnorm);
@@ -57,12 +57,12 @@ namespace ksi
         granular_dbscan(const granular_dbscan &obj);
 
         partition doPartition(const dataset &ds);
-        
-        granular_dbscan & operator=(const granular_dbscan &obj);
+
+        granular_dbscan &operator=(const granular_dbscan &obj);
 
         /** @return The method clones the object and returns the address of the copy. */
-        virtual partitioner * clone() const;
-        
+        virtual partitioner *clone() const;
+
         virtual ~granular_dbscan();
 
         /** @return an abbreviation of a method */
@@ -71,25 +71,24 @@ namespace ksi
     protected:
         std::vector<std::vector<std::shared_ptr<descriptor>>> prepareFuzzyData(const dataset &ds, std::shared_ptr<partitioner> algorithm);
 
-        std::size_t findMaxMembIndex(const std::size_t datasetSize, const std::vector<double> &memberships, const double mi2, const std::vector<bool> &processed);
+        std::size_t findMaxMembIndex(const std::size_t datasetSize, const std::vector<double> &memberships, const double _psi, const std::vector<bool> &processed);
 
-        std::vector<double> findNeighboursMemberships(const std::vector<std::vector<std::shared_ptr<descriptor>>> &ds, const std::vector<std::shared_ptr<descriptor>> &granule);
+        std::vector<double> findNeighboursMemberships(const std::vector<std::vector<std::shared_ptr<descriptor>>> &fuzzyDs, const std::vector<std::shared_ptr<descriptor>> &granule);
 
         const descriptor_triangular calculateDistance(
             const std::shared_ptr<descriptor> &firstDescriptor,
             const std::shared_ptr<descriptor> &secondDescriptor);
 
         const double areaPercentageInSpace(const double epsilon, const descriptor_triangular &triangle);
-        
+
         /** @return The method returns the membership of data item d to granule gs.
          *  @param gs granules: set of descriptors
          *  @param tnorm a tnorm
          *  @date   2022-09-23
          *  @author Krzysztof Siminski
          */
-        double getMembershipToGranule(const datum & d, const  std::vector<std::shared_ptr<descriptor>> & gs, const std::shared_ptr<t_norm>  tnorm);
-        
-        
+        double getMembershipToGranule(const datum &d, const std::vector<std::shared_ptr<descriptor>> &gs, const std::shared_ptr<t_norm> tnorm);
+
         /** @return The method returns the membership matrix. Each row represents a cluster. Each column represents input data item. The i-th cell in the j-th column in the matrix holds membership value of the j-th input data item to the i-th cluster.
          *  @param ds dataset: a set of input data items
          *  @param fuzzyDs granules: set of descriptors
@@ -97,8 +96,7 @@ namespace ksi
          *  @date   2022-09-23
          *  @author Krzysztof Siminski
          */
-        std::vector<std::vector<double>> getClusterDatumMatrix(const dataset & ds, const std::vector<std::vector<std::shared_ptr<descriptor>>> & fuzzyDs, const std::vector<std::vector<double>> & mU);
-
+        std::vector<std::vector<double>> getClusterDatumMatrix(const dataset &ds, const std::vector<std::vector<std::shared_ptr<descriptor>>> &fuzzyDs, const std::vector<std::vector<double>> &mU);
     };
 }
 #endif
