4e6725c0a94e18696b30ded3ca7b248836331bc6
Maks <maksbroja@gmail.com>
2023-10-11 14:16:26 +0200

    Dodalem metryke kosinusowa, manhattan i Chebysheva

---
+++ source/metrics/metric-Manhattan.cpp
@@ -0,0 +1,64 @@
+/** @file */ 
+
+#include <string>
+#include <sstream>
+#include <cmath>
+
+#include "../metrics/metric.h"
+#include "../metrics/metric-manhattan.h"
+#include "../service/debug.h"
+
+std::string ksi::metric_manhattan::getAbbreviation() const
+{
+    return { "Manhattan" };
+}
+
+
+ksi::metric_manhattan::~metric_manhattan()
+{
+}
+
+std::shared_ptr<ksi::metric> ksi::metric_manhattan::clone() const
+{
+   return std::shared_ptr<ksi::metric> (new ksi::metric_manhattan(*this));
+}
+
+double ksi::metric_manhattan::calculateDistance(const ksi::datum & l, const ksi::datum & p) const
+{
+   try
+   {
+       return calculateDistance(l.getVector(), p.getVector());
+   }
+   CATCH;
+}
+
+
+double ksi::metric_manhattan::calculateDistance(const std::vector<double>& l, const std::vector<double>& p) const
+{
+   try
+   {
+      // wyznaczam odleglosc Chebyszewa:
+      
+      auto lsize = l.size();
+      auto psize = p.size();
+      
+      if (lsize != psize)
+      {
+         std::stringstream ss;
+         ss << "Numbers of attributes do not match:" << std::endl;
+         ss << " for left:  " << lsize << std::endl;
+         ss << " for right: " << psize << std::endl;
+         
+         throw ksi::exception (ss.str());
+      }
+
+      double suma = 0.0;
+      for (std::size_t a = 0; a < lsize; a++)
+      {
+            suma += fabs(l[a] - p[a]);
+         
+      }
+      return suma;
+   }
+   CATCH;
+}
---
+++ source/metrics/metric-cosine.h
@@ -0,0 +1,39 @@
+/** @file */ 
+
+#ifndef METRIC_COSINE_H
+#define METRIC_COSINE_H
+
+#include <vector>
+#include <memory>
+
+#include "../common/number.h"
+#include "../common/datum.h"
+#include "../metrics/metric.h"
+
+namespace ksi
+{
+   /** Class for calculating a distance between data.
+    * @date 2018-01-04
+    * 
+     */
+   class metric_cosine : virtual public metric 
+   {
+   protected:
+      
+   public:
+      virtual ~metric_cosine ();
+      /** Just calculates a distance :-) */
+      virtual double calculateDistance(const datum & l, const datum & p) const override;
+      /** Just calculates a distance :-) */
+      virtual double calculateDistance(const std::vector<double> & l, const std::vector<double> & p) const override;
+      /** method for cloning an object */
+      std::shared_ptr< ksi::metric > clone() const;
+      
+      /** @return abbreviation of the metric 
+       @date 2021-02-01 */
+      virtual std::string getAbbreviation() const override;
+   };
+}
+
+#endif
+
