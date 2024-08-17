926799ce7da805862e724ce39d0b8ce958136f62
WojciechPtas <wojciech.ptas@gmail.com>
2024-08-17 11:34:54 +0200

    stan prac na 17.08.2024

--- source/neuro-fuzzy/three_way_decision_nfs.h
+++ source/neuro-fuzzy/three_way_decision_nfs.h
@@ -338,5 +338,28 @@ namespace ksi
        std::ostream & operator<< (std::ostream & ss, const ksi::three_way_decision_nfs & system);
    };
 }
+namespace ksi
+{
+    class three_way_decision_nfs_WP : public three_way_decision_nfs
+    {
+        protected:
+        std::function<std::tuple<double,double>(const std::vector<std::tuple<double, double, double>> &, const double&)> m_fitnessFunction;
+    public:
+        three_way_decision_nfs_WP (const std::vector<std::shared_ptr<ksi::neuro_fuzzy_system>> & cascade, 
+                              const std::string & train, 
+                              const std::string & validation,
+                              const std::string & test, 
+                              const std::string & result,
+                              std::function<std::tuple<double,double>(const std::vector<std::tuple<double, double, double>> &, const double&)> fitnessFunction);
+        ~three_way_decision_nfs_WP() override = default;
+        void createFuzzyRulebase(const ksi::dataset& train, const ksi::dataset & test, const ksi::dataset & validation) override;
+    };
 
+}
+namespace WP{
+    std::map<double, std::tuple<int,int>> create_prefixes(const std::vector<std::tuple<double, double, double>> & data);
+    std::tuple<double,double> get_noncommitment(const std::vector<std::tuple<double, double, double>> & data, const double& threshold);
+    std::tuple<double,double> get_noncommitment_based_on_threshold(const std::vector<std::tuple<double, double, double>> & data, const double& threshold);
+    std::tuple<double,double> get_noncommitment_based_on_threshold_hard_penalize(const std::vector<std::tuple<double, double, double>> & data, const double& threshold);
+}
 #endif
